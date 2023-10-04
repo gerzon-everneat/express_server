b58bcc2bc421a30268b1af8e754d9ace0ad53a63
gerzon-everneat <gerzon@everneat.co>
2023-10-05 00:58:55 +0800

    start

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // Serve static files from the 'dist' folder
-app.use(express.static("dist"));
+app.use(express.static("neatlist-FE\build"));
 
 app.listen(80, () => {
   console.log("Server is running on port 80");
